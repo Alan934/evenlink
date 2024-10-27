6dd9ed85a4a14fc1b2fe411ce7cda3b3b9f4d79d
Alan934 <sanjurjoalan77@gmail.com>
2024-10-26 21:55:24 -0300

    img wa and message

--- src/components/BurbujaWhatsApp.tsx
+++ src/components/BurbujaWhatsApp.tsx
@@ -3,14 +3,22 @@ import '../styles/BurbujaWhatsApp.css';
 
 const FloatingBubble = () => {
   const whatsappNumber = '542615406465';
+  const mensaje = 'Hola, me comunico con usted por una consulta';
 
   const handleClick = () => {
-    window.open(`https://wa.me/${whatsappNumber}`, '_blank');
+    window.open(`https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${mensaje}`, '_blank');
   };
 
   return (
     <div onClick={handleClick} className="floating-bubble">
-      💬
+      <img 
+        src="https://res.cloudinary.com/de9ojxknm/image/upload/v1722532720/Footer/bybzpetuzhkypkmlkwax.svg" 
+        alt="WhatsApp Icon" 
+        style={{
+          width: '60px',  // Ajusta el tamaño según tus necesidades
+          height: '60px',
+        }} 
+      />
     </div>
   );
 };
